7cf096f9b641e86dbf8c71d005a96e089e3fea72
prinsonnazareth2@gmail.com <prinsonnazareth2@gmail.com>
2026-01-07 15:52:34 +0530

    updates

--- learning001/learning001.c
+++ learning001/learning001.c
@@ -1,16 +1,54 @@
-#include <stdio.h>
+// #include <stdio.h>
 
+
+// int main(){
+
+//     int arr[] = {1,2,3,4,5};
+
+//     int length = sizeof(arr) / sizeof(arr[0]);
+
+//     for(int i=0 ; i<length ; i++){
+
+//         printf("%d",arr[i]);
+//     }
+
+//     return 0;
+// }
+
+
+#include<stdio.h>
 
 int main(){
 
-    int arr[] = {1,2,3,4,5};
+    int a,b,c;
+    char op;
 
-    int length = sizeof(arr) / sizeof(arr[0]);
 
-    for(int i=0 ; i<length ; i++){
+    printf("Enter the first element");
+    scanf("%d",&a);
 
-        printf("%d",arr[i]);
+    printf("Enter the operator");
+    scanf("%s",&op);
+
+    printf("Enter the second element");
+    scanf("%d",&b);
+
+
+    switch(op){
+        case '+':
+            printf("%d",c=a+b);
+            break;
+        case '/':
+            if(b != 0){
+                printf("%d",c=a/b);
+            }else{
+                printf("cant divide");
+            }
+            break;
+        default:
+            printf("invalid");
     }
 
     return 0;
+
 }
---
+++ learning006/file.c
@@ -0,0 +1,51 @@
+// #include <stdio.h>
+
+// int main() {
+//     FILE *fp;
+//     char ch;
+
+//     // Open file in read mode
+//     fp = fopen("file.txt", "r");
+
+//     // Check if file opened successfully
+//     if (fp == NULL) {
+//         printf("Error: Cannot open file.\n");
+//         return 1;
+//     }
+
+//     // Read file character by character (Sequential Access)
+//     while ((ch = fgetc(fp)) != EOF) {
+//         printf("%c", ch);
+//     }
+
+//     // Close the file
+//     fclose(fp);
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    int roll;
+    char name[20];
+    float marks;
+
+    fp = fopen("data.txt", "r");
+
+    if (fp == NULL) {
+        printf("File not found");
+        return 0;
+    }
+
+    fscanf(fp, "%d %s %f", &roll, name, &marks);
+
+    printf("Roll: %d\n", roll);
+    printf("Name: %s\n", name);
+    printf("Marks: %.2f\n", marks);
+
+    fclose(fp);
+    return 0;
+}
+
